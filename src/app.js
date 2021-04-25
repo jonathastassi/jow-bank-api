df1dd94498dedfdf0bb78db4374d252df3f6be50
Jonathas Silva <jonathastassi@hotmail.com>
2021-04-25 08:43:00 -0300

    User routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 
-// const router = require('./routes')
+const router = require('./routes')
 
 const app = express()
+
+app.use('/api', router)
 
 app.use(express.json())
 
