b66af3d80a8d016679c0caf01c9f5e60ebc39177
Jonathas Silva <jonathastassi@hotmail.com>
2021-04-30 09:12:51 -0300

    Improve functional programming concepts - immutability

--- src/domains/commands/users/register_user.js
+++ src/domains/commands/users/register_user.js
@@ -2,8 +2,8 @@ const hashBcrypt = require('../../utils/hash_bcrypt')
 
 module.exports = (repository) => {
   const call = (name, email, password, photo, cellphone) => {
-    password = hashBcrypt(password)
-    return repository.create(name, email, password, photo, cellphone)
+    const passwordEncripted = hashBcrypt(password)
+    return repository.create(name, email, passwordEncripted, photo, cellphone)
   }
 
   return {
