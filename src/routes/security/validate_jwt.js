517e3de1b1e63d5dc5b90e6716947c31332a6e12
Jonathas Silva <jonathastassi@hotmail.com>
2021-05-03 10:51:33 -0300

    Added token JWT validation

---
+++ src/routes/security/validate_jwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const validateJWT = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
+
+  const bearer = token.split(' ')
+  const bearerToken = bearer[1]
+
+  jwt.verify(bearerToken, process.env.SECRET, function (err, decoded) {
+    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' })
+    req.userId = decoded.payload.id
+    req.userEmail = decoded.payload.email
+    next()
+  })
+}
+
+module.exports = validateJWT
