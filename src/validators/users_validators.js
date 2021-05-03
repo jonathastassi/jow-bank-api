b3e6d3aa150e56ebb15c4163eb5b27b2994eabaf
Jonathas Silva <jonathastassi@hotmail.com>
2021-05-03 09:34:34 -0300

    Validate user register

---
+++ src/validators/users_validators.js
@@ -0,0 +1,14 @@
+
+const { celebrate, Joi, Segments } = require('celebrate')
+
+module.exports = {
+  validateRegister: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required().max(255),
+      email: Joi.string().email().required().max(255),
+      password: Joi.string().required().max(255),
+      photo: Joi.string().max(255),
+      cellphone: Joi.string().max(20)
+    })
+  })
+}
