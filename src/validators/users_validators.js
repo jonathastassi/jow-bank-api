0bd4bba5aea15e2ee9d68609f2f7b039f8b34f6f
Jonathas Silva <jonathastassi@hotmail.com>
2021-05-03 10:47:04 -0300

    validate login

--- src/validators/users_validators.js
+++ src/validators/users_validators.js
@@ -10,5 +10,11 @@ module.exports = {
       photo: Joi.string().max(255),
       cellphone: Joi.string().max(20)
     })
+  }),
+  validateLogin: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required().max(255),
+      password: Joi.string().required().max(255)
+    })
   })
 }
