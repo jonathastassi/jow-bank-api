8eeb937d742cc6b257e5f25dafc21518f0fcaeb6
Jonathas Silva <jonathastassi@hotmail.com>
2021-05-03 10:47:34 -0300

    created a method to find itens in DB

--- src/repositories/user_repository.js
+++ src/repositories/user_repository.js
@@ -2,6 +2,10 @@ const create = (database) => (user) => database('users').insert(user)
   .returning(['name', 'email', 'photo', 'cellphone'])
   .then(rows => rows[0])
 
+const findBy = (database) => ({ field, value }) => database('users').where(field, value)
+  .then(rows => rows[0])
+
 module.exports = (repository) => ({
-  create: create(repository)
+  create: create(repository),
+  findBy: findBy(repository)
 })
