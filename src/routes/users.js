d14c459db0fda28ae1b2cf12b2da962123375f53
Jonathas Silva <jonathastassi@hotmail.com>
2021-05-03 10:51:33 -0300

    Added token JWT validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,10 +3,11 @@ const router = express.Router()
 
 const controller = require('../controllers/users_controller')
 const validator = require('../validators/users_validators')
+const validateJWT = require('../routes/security/validate_jwt')
 
 router.post('/register', validator.validateRegister, controller.register)
-router.post('/login', controller.login)
-router.get('/', controller.getUserLogged)
+router.post('/login', validator.validateLogin, controller.login)
+router.get('/', validateJWT, controller.getUserLogged)
 router.put('/', controller.update)
 
 module.exports = router
